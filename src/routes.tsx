50b1313cde86147aba326be1f441bc078494f60a
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-06-24 19:12:55 -0300

    feat: create new Room page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 export function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Route path="/rooms/new" exact component={NewRoom} />
+        <Route path="/rooms:id" component={Room} />
       </Switch>
     </BrowserRouter>
   );
