c6a086b498c9ed9613ae086fea0813349b0615e4
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-06-25 19:14:13 -0300

    chore: routes little adjustments

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import { AdminRoom } from "./pages/AdminRoom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
@@ -11,6 +12,7 @@ export function Routes() {
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" exact component={NewRoom} />
         <Route path="/rooms/:id" component={Room} />
+        <Route path="/admin/rooms/:id" component={AdminRoom} />
       </Switch>
     </BrowserRouter>
   );
